0977a3c3452496e44bbbb170f25ea1e4bbff616a
uday4002 <udaykiranbudda9@gmail.com>
2025-01-09 08:04:48 +0530

    Fix npm error TAR_BAD_ARCHIVE

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {HashRouter as Router,Routes,Route} from 'react-router-dom'
 import WhatWeDo from './components/WhatWeDo'
 import WhyChooseUs from './components/WhyChooseUs';
 import Contact from './components/Contact'
@@ -9,14 +9,14 @@ import Contact from './components/Contact'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/what-we-do" element={<WhatWeDo />} />
         <Route path="/colleges" element={<WhyChooseUs />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
 
